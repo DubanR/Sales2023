76779f974ee97b79b6638a6dc131f57e4b043260
Duban Ramirez <duban.rq@gmail.com>
2023-02-25 17:35:27 -0500

    Add categories table

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -3,7 +3,6 @@ using Sales.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
