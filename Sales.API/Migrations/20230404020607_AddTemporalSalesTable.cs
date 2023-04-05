d64a67d920b91a8f63a889344ff95c7fb88bb5a5
Duban Ramirez <duban.rq@gmail.com>
2023-04-04 20:17:01 -0500

    Add products to cart

---
+++ Sales.API/Migrations/20230404020607_AddTemporalSalesTable.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sales.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTemporalSalesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TemporalSales",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    userId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    productId = table.Column<int>(type: "int", nullable: false),
+                    quantity = table.Column<float>(type: "real", nullable: false),
+                    remarks = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemporalSales", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_TemporalSales_AspNetUsers_userId",
+                        column: x => x.userId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_TemporalSales_Products_productId",
+                        column: x => x.productId,
+                        principalTable: "Products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemporalSales_productId",
+                table: "TemporalSales",
+                column: "productId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemporalSales_userId",
+                table: "TemporalSales",
+                column: "userId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TemporalSales");
+        }
+    }
+}
